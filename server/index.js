1851543be687007443d7af78aad54bed39f113f4
HoangHieu <dinhhoanghieu321@gmail.com>
2022-07-12 22:15:22 +0700

    Create Project & NodeJS Server & Signin screen

---
+++ server/index.js
@@ -0,0 +1,17 @@
+// IMPORTS FROM PACKAGES
+const express = require("express");
+
+// IMPORTS FROM OTHER FILES
+const authRouter = require("./routes/auth");
+
+// INIT
+const PORT = 3000;
+const app = express();
+
+// MIDDLEWARE
+app.use(authRouter);
+
+
+app.listen(PORT, () => {
+    console.log(`connected at port ${PORT}`);
+});
